7bfe146349414d4d015a32d3de83f12cd7a828b2
tadakalasameer <119431765+tadakalasameer@users.noreply.github.com>
2022-12-21 02:28:34 -0800

    Add files via upload

---
+++ pig.js
@@ -0,0 +1,30 @@
+function change1() {
+    document.getElementById("reply1").style.display = "block";
+    document.getElementById('box1').src = "img/cbox.png";
+    document.getElementById('restext1').innerHTML = 'The finest period to visit India depends on where you desire to go and what you desire to do as India has a diverse geographical region. The southern zones are the good destinations in the winter months (November to March), as temperatures are mild and game viewing in the national parks will be the best options. Comparatively winter temperatures in the north may become harsh, but this is an excellent time for bird watching, as the migrant species arrive in this period. Summer weather (April to June) in the south is mild and this is the perfect time for beach holiday or cultural tours in this region. Whereas in the north region, summers are an excellent time to visit the Himalayas. Although the temperature will be high, summer is considered a good time for game viewing.';
+}
+
+function change2() {
+    document.getElementById("reply2").style.display = "block";
+    document.getElementById('box2').src = "img/cbox.png";
+    document.getElementById('restext2').innerHTML = "India is as secure as any other foreign location you visit for the first time. Indians are very hospitable and helpful people. Moreover, we will be there to take care of you in every step of your way and to instruct certain basic precautions. Like it is a very good idea to lock away your spare currency, traveler’s cheques, passports and precious jewellery in the hotel safe. Moreover, be aware while walking around alone at late night, particularly in the deserted areas or small towns because everything closes early. If possible, avoid travelling late night and never leave your luggage unattended at the airport or train station.";
+}
+
+function change3() {
+    document.getElementById("reply3").style.display = "block";
+    document.getElementById('box3').src = "img/cbox.png"
+    document.getElementById('restext3').innerHTML = "Everyone except nationals of Bhutan, Maldives and Nepal needs a visa to visit India. This must be obtained before arrival and we recommend that you contact your local Indian embassy or consulate when planning your trip."
+
+}
+
+function change4() {
+    document.getElementById("reply4").style.display = "block";
+    document.getElementById('box4').src = "img/cbox.png"
+    document.getElementById('restext4').innerHTML = 'The Indian currency is the rupee, which is made of 100 paisa. Major currencies such as US dollars, British pounds and Euros are easily exchanged throughout India. Most international airports of the country have the money exchange counters, and also some selected larger hotels and resorts have this facility to their customer..'
+}
+
+function change5() {
+    document.getElementById("reply5").style.display = "block";
+    document.getElementById('box5').src = "img/cbox.png"
+    document.getElementById('restext5').innerHTML = 'In the hot Indian summers we recommend light cotton tops, shorts and trousers. If you are travelling to any of India’s national parks, remember to pack some long-sleeved cotton tops for game drives. In the cold winter months, temperature may drop drastically, therefore warm clothing would be essential. As Indian landscape is diverse we advise to include the clothes by taking care of the weather and temperature of the area on the particular season. Besides, when visiting religious monuments and shrines you may need to dress formally, so both men and women should pack some long skirts and pants.'
+}
